d4dadf90c6f481ac42e31ce62c0cce2ee31fae2a
flino1934 <f.lino1934@hotmail.com>
2023-09-11 18:22:21 -0300

    Create structure project

---
+++ src/main/java/com/lino/dscatalog/entity/Category.java
@@ -0,0 +1,59 @@
+package com.lino.dscatalog.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String name;
+
+	public Category() {
+	}
+
+	public Category(long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return id == other.id;
+	}
+
+}
